ace1a67591d71af22447878676cb59b979e38064
Shreyas Hiru Vanage <115716876+Shreyas-Vanage@users.noreply.github.com>
2023-03-15 19:30:38 +0530

    Add files via upload

---
+++ OTP_Verification.py
@@ -0,0 +1,22 @@
+import os
+import math
+import random
+import smtplib
+
+digits="0123456789"
+temp_OTP=""
+#otp=".join([str(random(0,9)for i in range(6))])"
+for i in range(6):
+    temp_OTP+=digits[math.floor(random.random()*10)]
+otp = temp_OTP + " is your OTP"
+msg= otp
+s = smtplib.SMTP('smtp.gmail.com',587)
+s.starttls()
+s.login("vanageshreyu@gmail.com", "lppspspreksowdkw")
+emailid = input("Enter your email: ")
+s.sendmail('vanageshreyu@gmail.com',emailid,msg)
+a = input("Enter Your OTP >>: ")
+if a==OTP:
+    print("Verified")
+else:
+    print("Please Check your OTP again")
